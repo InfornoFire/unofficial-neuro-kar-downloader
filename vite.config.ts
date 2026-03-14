733c4a94db66c8161c4fadcbae309cfc7488d05c
InfornoFire <16070849+InfornoFire@users.noreply.github.com>
2026-03-14 03:36:34 -0400

    fix: resolve page paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/unofficial-neuro-kar-downloader/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
